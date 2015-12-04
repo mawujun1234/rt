96dab388effe7a3e4f54e602e38191c8254ab4d7
mawujun1234 <mawujun1234@163.com>
2015-12-04 16:20:13 +0800

    1111

---
+++ src/main/java/com/mawujun/family/FamilyController.java
@@ -0,0 +1,30 @@
+package com.mawujun.family;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FamilyController {
+	@RequestMapping("/family/getShop.do")
+	public List<ShopInfo> getShop(){
+		List<ShopInfo> list=new ArrayList<ShopInfo>(0);
+		for (int i=0;i<100;i++){
+			ShopInfo info=new ShopInfo();
+			info.setShopnm("展销中心");
+			info.setDetails("展销中心");
+			info.setRanking(i);
+			info.setCol(i%20+1);
+			info.setRow(i/20+1);
+			
+			info.setNum(new Double((Math.random()*400)+"").intValue());
+			info.setMoney(Math.ceil(Math.random()*1000*100)/100);
+			
+			list.add(info);
+		}
+		return list;
+	}
+
+}
---
+++ src/main/java/com/mawujun/family/ShopInfo.java
@@ -0,0 +1,57 @@
+package com.mawujun.family;
+
+public class ShopInfo {
+	
+	private String shopnm;
+	private String details;
+	private Integer ranking;
+	private Integer col;
+	private Integer row;
+	
+	private Integer num;
+	private Double money;
+	public String getShopnm() {
+		return shopnm;
+	}
+	public void setShopnm(String shopnm) {
+		this.shopnm = shopnm;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public Integer getCol() {
+		return col;
+	}
+	public void setCol(Integer col) {
+		this.col = col;
+	}
+	public Integer getRow() {
+		return row;
+	}
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	public Integer getRanking() {
+		return ranking;
+	}
+	public void setRanking(Integer ranking) {
+		this.ranking = ranking;
+	}
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public Double getMoney() {
+		return money;
+	}
+	public void setMoney(Double money) {
+		this.money = money;
+	}
+
+}
